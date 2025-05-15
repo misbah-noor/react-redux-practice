b6d83833b3a2f2fbafb760d79a9ac475a07c1b75
Misbah <misbahnoor3252@gmail.com>
2025-05-15 17:13:24 +0500

    basics

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice';
+
+export default function Counter(){
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return(
+        <div className='flex flex-col items-center gap-4 mt-10 text-xl '>
+            <p>Count: {count}</p>
+            <div className='flex gap-3'>
+             <button onClick={()=>dispatch(increment())} className='bg-green-500 px-4 py-1 rounded'>+</button>
+                 <button onClick={()=>dispatch(decrement())} className='bg-red-500 px-4 py-1 rounded'>-</button>
+            </div>
+        </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>
 )
